9d48bad5a67402d9dd92a452a670db401898776f
Manav Bansal <bansalmanav39@gmail.com>
2025-09-23 15:19:10 -0700

    Set up router dom for frontend client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar'
+import HomePage from './Pages/HomePage'
+import SignUpPage from './Pages/SignUpPage'
+import LoginPage from './Pages/LoginPage'
+import SettingsPage from './Pages/Settings'
+import ProfilePage from './Pages/ProfilePage'
 
-function App() {
 
+const App = () => {
   return (
-    <>
-      General Chat App
-    </>
+    <div>
+       <Navbar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/signup' element={<SignUpPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/settings' element={<SettingsPage/>}/>
+          <Route path='/profile' element={<ProfilePage/>}/>
+        </Routes>
+
+    </div>
   )
 }
 
