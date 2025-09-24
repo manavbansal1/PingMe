e92817187904694b96bebc637b1bff433fa310dd
Manav Bansal <bansalmanav39@gmail.com>
2025-09-23 21:32:25 -0700

    Added code for profile picture update

--- client/src/Pages/ProfilePage.jsx
+++ client/src/Pages/ProfilePage.jsx
@@ -3,12 +3,23 @@ import { Camera, Mail, User } from "lucide-react";
 import { useAuthStore } from '../store/useAuthStore.js'
 
 const ProfilePage = () => {
-  const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
-   const [selectedImg, setSelectedImg] = useState(null);
-  const handleImageUpload = async(e) => {
-    
-  }
 
+  const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const handleImageUpload = async(e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      
+      reader.onload = async () => {
+        const base64data = reader.result;
+        setSelectedImg(base64data);
+        await updateProfile({profilePicture : base64data});
+      }
+    }
+  }
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
@@ -21,7 +32,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={selectedImg || authUser.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilePicture || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -63,6 +63,17 @@ export const useAuthStore = create((set) => ({
 
     updateProfile: async (data) => {
         set({isUpdatingProfile : true});
+        try {
+            const response = await axiosInstance.put('/auth/update-profile', data);
+            set({authUser : response.data});
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            console.log("Error in updateProfile : ", error);
+            toast.error(error?.response?.data?.message || "Error in updating profile");
+        }
+        finally {
+            set({isUpdatingProfile : false});
+        }
     }
 
 }));
